45f17791e4b56356ca1bef6715cb3fe196e104ad
Charlie Tanksley <charlie.tanksley@gmail.com>
2011-12-19 11:44:39 -0500

    trying to sort out mobile orgmode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,6 +20,11 @@
 (set-fringe-style -1)
 (tooltip-mode -1)
 
+(setq org-directory "~/org")
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
+(setq org-mobile-inbox-for-pull "~/Dropbox/inbox.org")
+
+
 (set-frame-font "Menlo-16")
 (load-theme 'tango-dark)
 ; COLORSCHEME
@@ -106,3 +111,15 @@
 
 ; Try:
 ; - ido-hacks
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/index.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
