fd9baf8067b59fbe2d76a963468a69613ba019ad
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-07-30 13:44:19 -0400

    add j-mode for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -207,6 +207,16 @@
 ;; ;;; This is the binary name of my scheme implementation
 ;; (setq scheme-program-name "csi")
 
+;;;;;;;;;
+;;; J ;;;
+;;;;;;;;;
+
+(autoload 'j-mode "j-mode.el"  "Major mode for J." t)
+(autoload 'j-shell "j-mode.el" "Run J from emacs." t)
+(setq auto-mode-alist
+            (cons '("\\.ij[rstp]" . j-mode) auto-mode-alist))
+(setq j-command "~/j701/bin/jconsole")
+
 
 ;;;;;;;;;;;;;
 ;; Clojure ;;
