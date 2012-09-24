7e7474ffb051a9a703069feae0e0547759efb3f3
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-09-24 13:39:39 -0400

    working version :

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,7 +62,7 @@
    (:name perspective           ; workspaces
     :after (progn
       (require 'perspective)
-      (persp-mode)))))
+      (persp-mode))))))
 
 ;; now set our own packages
 (setq my:el-get-packages
@@ -128,7 +128,7 @@
 
 (set-frame-font "Inconsolata-16")
 ;(load-theme 'tango-dark)
-; (load-theme 'twilight-anti-bright-theme)
+(load-theme 'twilight-anti-bright)
 
 
 ; I want to be sure to use the right keybindings
@@ -180,3 +180,15 @@
 ; ;; Your init file should contain only one such instance.
 ; ;; If there is more than one, they won't work right.
 ; )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("159bb8f86836ea30261ece64ac695dc490e871d57107016c09f286146f0dae64" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
