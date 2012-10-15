450878f29b4980e52877f4ccf9838d0d839665ea
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-10-15 09:14:17 -0400

    have some style

---
+++ .emacs.d/init.d/my-styles.el
@@ -0,0 +1,8 @@
+(progn
+  (setq custom-safe-themes t)
+  (load-theme 'twilight-anti-bright)
+  ;(load-theme 'zenburn)
+  (set-face-attribute 'default nil
+                      :family "Monaco" :height 145 :weight 'normal))
+
+(provide 'my-styles)
