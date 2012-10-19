e3a3b88145d13e3aedc2703274cc355e6e994a2e
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-10-19 08:44:47 -0400

    move customizations into their own file and ignore them

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -309,18 +309,5 @@
 ;; (require 'auto-complete-config)
 ;; (ac-config-default)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" "159bb8f86836ea30261ece64ac695dc490e871d57107016c09f286146f0dae64" default)))
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "smtp.gmail.com")
- '(smtpmail-smtp-service 587)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file "~/.emacs.d/init.d/emacs-custom.el")
+(load custom-file 'noerror)
