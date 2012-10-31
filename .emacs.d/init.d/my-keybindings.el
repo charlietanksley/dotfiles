0b9afb0b19abd42ecbe2438c1f3241585a717c07
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-10-31 12:38:00 -0400

    move keybindings out to their own file

---
+++ .emacs.d/init.d/my-keybindings.el
@@ -0,0 +1,18 @@
+;; I don't need to kill emacs that easily
+;; the mnemonic is C-x REALLY QUIT
+(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "C-x C-c") 'delete-frame)
+
+;; region manipulation
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; Completion that uses many different methods to find options.
+(global-set-key (kbd "C-.") 'hippie-expand)
+(global-set-key (kbd "C-:") 'hippie-expand-lines)
+
+(provide 'my-keybindings)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,8 +6,6 @@
 (turn-on-guru-mode)
 
 (setq smex-save-file "~/.emacs.d/.smex-items")
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 (autoload 'ack-and-a-half-same "ack-and-a-half" nil t)
 (autoload 'ack-and-a-half "ack-and-a-half" nil t)
@@ -19,26 +17,21 @@
 (defalias 'ack-find-file 'ack-and-a-half-find-file)
 (defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
 
-;; region manipulation
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
 (require 'wrap-region)
-(wrap-region-mode t)
+(wrap-region-global-mode t)
+;; Don't screw up key bindings in magit-mode
+(add-to-list 'wrap-region-except-modes 'magit-status-mode)
 
 ;; Represent undo-history as an actual tree (visualize with C-x u)
 (setq undo-tree-mode-lighter "")
 (require 'undo-tree)
 (global-undo-tree-mode)
 
-;; I don't need to kill emacs that easily
-;; the mnemonic is C-x REALLY QUIT
-(global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
-(global-set-key (kbd "C-x C-c") 'delete-frame)
-
 (mapc 'require '(my-paredit
                  my-cucumber
                  my-hippie
                  my-ido
+                 my-keybindings
                  my-magit
                  my-markdown
                  my-ruby
