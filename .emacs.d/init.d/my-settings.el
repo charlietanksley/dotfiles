67e97640f3d779b37ebd8ae1cab4490a3225db16
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-10-31 12:25:26 -0400

    move settings out to a file

---
+++ .emacs.d/init.d/my-settings.el
@@ -0,0 +1,55 @@
+(progn
+  ; No startup message
+  (setq inhibit-startup-message t)
+  ; Quiet that bell!
+  (setq visible-bell 1)
+
+  ; Whitespace
+  (setq whitespace-style '(face trailing lines-tail tabs)
+        whitespace-line-column 80)
+
+  ; Use iswitchb
+  (iswitchb-mode t)
+
+  ; Advanced stuff Emacs disables by default
+  (put 'narrow-to-region 'disabled nil)
+
+  ; This is a tricky one.  I want for the right (and only the right) command key to function as a control key
+  (setq mac-right-command-modifier 'control)
+  (setq mac-allow-anti-aliasing t)
+
+  ; Abbreviate 'yes or no' prompt to 'y or n'
+  (fset 'yes-or-no-p 'y-or-n-p)
+
+  ; Appearance stuff: no scroll bars or tool bars, make cursor blink
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (blink-cursor-mode t)
+  ; Show/highlight matching paren
+  (show-paren-mode t)
+  ; No clue what these two do
+  (set-fringe-style -1)
+  (tooltip-mode -1)
+  (global-auto-revert-mode t)
+
+  ; We don't need any extra backup files
+  (setq make-backup-files nil)
+  ; Don't save every file-visiting buffer
+  (setq auto-save-default nil)
+
+  (set-default 'indent-tabs-mode nil)
+  (set-default 'indicate-empty-lines t)
+  (set-default 'imenu-auto-rescan t)
+
+  ;; Also auto refresh dired, but be quiet about it
+  (setq global-auto-revert-non-file-buffers t)
+  (setq auto-revert-verbose nil)
+
+  ;; Sentences do not need double spaces to end. Period.
+  (set-default 'sentence-end-double-space nil)
+
+  ;; Add parts of each file's directory to the buffer name if not unique
+  (require 'uniquify)
+  (setq uniquify-buffer-name-style 'forward))
+
+(provide 'my-settings)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,10 +2,6 @@
 (require 'my-paths)
 (require 'my-packages)
 
-(require 'campfire)
-
-; config that doesn't really belong here
-
 (require 'guru-mode)
 (guru-mode t)
 
@@ -29,24 +25,6 @@
 (require 'wrap-region)
 (wrap-region-mode t)
 
-; Generic emacs configuration
-
-; We don't need any extra backup files
-(setq make-backup-files nil)
-; Don't save every file-visiting buffer
-(setq auto-save-default nil)
-
-(set-default 'indent-tabs-mode nil)
-(set-default 'indicate-empty-lines t)
-(set-default 'imenu-auto-rescan t)
-
-; A few things from Emacs Rocks
-;; Auto refresh buffers
-(global-auto-revert-mode 1)
-
-;; Also auto refresh dired, but be quiet about it
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
 ;; org-mode: Don't ruin S-arrow to switch windows please (use M-+ and M-- instead to toggle)
 (setq org-replace-disputed-keys t)
 
@@ -58,61 +36,12 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 
-;; Sentences do not need double spaces to end. Period.
-(set-default 'sentence-end-double-space nil)
-
-;; Add parts of each file's directory to the buffer name if not unique
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-
 ;; I don't need to kill emacs that easily
 ;; the mnemonic is C-x REALLY QUIT
 (global-set-key (kbd "C-x r q") 'save-buffers-kill-terminal)
 (global-set-key (kbd "C-x C-c") 'delete-frame)
 
 ; back to me
-
-;; (require 'perspective)
-
-;; ;; Enable perspective mode
-;; (persp-mode t)
-
-;(add-hook 'text-mode-hook 'turn-on-auto-fill)
-;(add-hook 'text-mode-hook 'turn-on-flyspell)
-; No startup message
-(setq inhibit-startup-message t)
-; Quiet that bell!
-(setq visible-bell 1)
-
-; Whitespace
-(setq whitespace-style '(face trailing lines-tail tabs)
-      whitespace-line-column 80)
-
-; Use iswitchb
-(iswitchb-mode t)
-
-; Advanced stuff Emacs disables by default
-(put 'narrow-to-region 'disabled nil)
-
-; This is a tricky one.  I want for the right (and only the right) command key to function as a control key
-(setq mac-right-command-modifier 'control)
-(setq mac-allow-anti-aliasing t)
-
-; Abbreviate 'yes or no' prompt to 'y or n'
-(fset 'yes-or-no-p 'y-or-n-p)
-
-; (delete-selection-mode t)
-; Appearance stuff: no scroll bars or tool bars, make cursor blink
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(blink-cursor-mode t)
-; Show/highlight matching paren
-(show-paren-mode t)
-; No clue what these two do
-(set-fringe-style -1)
-(tooltip-mode -1)
-(global-auto-revert-mode t)
-
 
 (mapc 'require '(my-paredit
                  my-cucumber
@@ -121,6 +50,7 @@
                  my-markdown
                  my-ruby
                  my-secrets
+                 my-settings
                  my-styles
                  my-irc
                  my-mu4e
