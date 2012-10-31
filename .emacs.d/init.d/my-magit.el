25e667c5ce62b187b6d2d00f9d220a9da58354a6
Charlie Tanksley <charlie.tanksley@gmail.com>
2012-10-31 12:01:54 -0400

    turn on auto-fill for magit commit messages

--- .emacs.d/init.d/my-magit.el
+++ .emacs.d/init.d/my-magit.el
@@ -1,10 +1,11 @@
 (progn
 ; Subtler highlight
   (global-set-key (kbd "C-x g") 'magit-status)
-  ; (set-face-background 'magit-item-highlight "#121212")
+    ; (set-face-background 'magit-item-highlight "#121212")
   ; (set-face-foreground 'diff-context "#666666")
   ; (set-face-foreground 'diff-added "#00cc33")
   ; (set-face-foreground 'diff-removed "#ff0000")
-  (setq magit-remote-ref-format 'remote-slash-name))
+  (setq magit-remote-ref-format 'remote-slash-name)
+  (add-hook 'magit-log-mode-hook 'turn-on-auto-fill))
 
 (provide 'my-magit)
