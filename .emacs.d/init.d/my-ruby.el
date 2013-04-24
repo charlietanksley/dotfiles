3fe17b878fb4aa21e2236fe62d37aa23e561084a
Charlie Tanksley <charlie.tanksley@gmail.com>
2013-04-23 20:38:58 -0400

    jbuilder files are ruby
    THEY'RE RUBY!!!!

--- .emacs.d/init.d/my-ruby.el
+++ .emacs.d/init.d/my-ruby.el
@@ -64,6 +64,7 @@
   (add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("\\.jbuilder$" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
